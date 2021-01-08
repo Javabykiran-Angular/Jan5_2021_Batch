758a44126e3f01211444c8afa10b8649b4070966
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-01-08 09:28:26 +0530

    Day4

---
+++ Typescript/Day4/demo1.js
@@ -0,0 +1,55 @@
+// if , if-else, nested if else
+// for loop, while loop, do while,foreach
+//switch
+var num = 3;
+if (num < 4) {
+    // console.log("Condition is true");
+}
+else {
+    // console.log("Condition is false");
+}
+if (3 < 4) {
+}
+else if (3 > 4) {
+}
+else {
+}
+//while loop
+while (num != 0) {
+    // console.log("Value of num is "+num);
+    num--;
+}
+// do{
+//     //logic
+//     num--;
+// }while(num!=0);
+for (var i = 0; i < 4; i++) {
+    // console.log(`
+    // ===========================
+    // Value of i is ${i} 
+    // -----------------------------
+    // `);
+}
+console.log("Value of i is " + i);
+for (var j = 0; j < 4; j++) {
+    // console.log(`
+    // ===========================
+    // Value of j is ${j}     
+    // `);    
+}
+// console.log("Value of j is "+j);
+var pi = 3.14;
+console.log("Value pi is " + pi);
+var choice = 31;
+switch (choice) {
+    case 1:
+        console.log("U are in case 1...");
+        break;
+    case 2:
+        console.log("U are in case 2...");
+        break;
+    case 3:
+        console.log("U are in case 3...");
+        break;
+    default: console.log("U are in default case ...");
+}
---
+++ Typescript/Day4/demo2.ts
@@ -0,0 +1,41 @@
+//Functions user defined
+//add1();
+// 4 basic types
+//1 function without argument & without Return
+//2 function with argument & without Return
+//3 function without argument & with Return
+//4 function with argument & with Return
+
+var i=10;
+var j=21;
+var x:number;
+function add1(){
+    const p=3.14;
+   // console.log("In Addition is "+(3+4));
+    var i=9;
+   // console.log("In Function Value of i is "+i);
+    j=25;
+    console.log("In Function Value of j is "+j);
+
+    x=20;
+    console.log("In Function Value of x is "+x);
+
+}
+
+//console.log("Value of i is "+i);
+// console.log("Value of j is "+j);
+
+// console.log("Value of x is "+x);
+
+// add1();
+
+// console.log("Value of pi is "+p);
+
+function add2(a:number,b:number){
+    console.log("Addition is "+(a+b));
+}
+
+add2(2,5);
+
+
+
